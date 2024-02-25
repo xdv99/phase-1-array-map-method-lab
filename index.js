2befdf8a824ef3e96589ef505585085e2458b6ca
Dawood Alkawaz <dalkawaz@gmail.com>
2024-02-25 13:46:57 +0300

    Solution

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    words = words.map(word => {
+      return (word[0].toUpperCase() + word.slice(1));
+    })
+    return words.join(' ');
+  });
 }
+
+console.log(titleCased());
